907271befc75ce4a62ce924f755bae2e4b66030f
NatradaChairat <natrada_chairat@cmu.ac.th>
2018-04-17 17:14:03 +0700

    RecipeActivityTest.java : Add success test method for test ui

--- app/src/androidTest/java/workshop/testing/ui/recipe/RecipeActivityTest.java
+++ app/src/androidTest/java/workshop/testing/ui/recipe/RecipeActivityTest.java
@@ -2,10 +2,13 @@ package workshop.testing.ui.recipe;
 
 
 
+import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.security.Key;
 
 import workshop.testing.R;
 
@@ -14,14 +17,25 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+import static workshop.testing.ui.recipe.RecipeActivity.KEY_ID;
 
 public class RecipeActivityTest {
 
     @Rule
-    public ActivityTestRule<RecipeActivity> activity = new ActivityTestRule(RecipeActivity.class);
+    public ActivityTestRule<RecipeActivity> activity
+            = new ActivityTestRule(RecipeActivity.class, true, false);
 
     @Test
-    public void openActivityByDefault() {
+    public void openActivityByDefaultShouldShowRecipenotfound() {
+        activity.launchActivity(new Intent());
         onView(withId(R.id.description)).check(matches(withText("Recipe not found.")));
+    }
+
+    @Test
+    public void success(){
+        Intent intent = new Intent();
+        intent.putExtra(KEY_ID, "chocolate_pudding");
+        activity.launchActivity(intent);
+        onView(withId(R.id.title)).check(matches(withText("Chocolate Pudding")));
     }
 }
