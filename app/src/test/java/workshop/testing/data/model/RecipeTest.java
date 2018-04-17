8b3dbf57796ff8df8d896034abc77e113d83c0e2
NatradaChairat <natrada_chairat@cmu.ac.th>
2018-04-17 16:44:00 +0700

    ReceipesTest.java: Add mixed.txt Test

--- app/src/test/java/workshop/testing/data/model/RecipeTest.java
+++ app/src/test/java/workshop/testing/data/model/RecipeTest.java
@@ -37,6 +37,26 @@ public class RecipeTest {
 
     }
 
+    @Test
+    public void i_can_read_data_from_mixed(){
+        //Call file
+        InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/mixed.txt");
+        //Call Recipe class to read the file
+        Recipe recipe = Recipe.readFromStream(inputStream);
+
+
+        assertNotNull(recipe);
+        assertEquals("punch", recipe.id);
+        assertEquals("Punch", recipe.title);
+        assertEquals("Juice of 3 lemons\n" +
+                "1 orange\n" +
+                "1 pint grape juice\n" +
+                "1 cup sugar\n" +
+                "1 cup water\n" +
+                "1 pine apple juice\n" +
+                "Mix all together and strain. Add large piece of ice.", recipe.description);
+
+    }
 
 
 
